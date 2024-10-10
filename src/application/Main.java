033b1619de1376669b432a22254ba40288cb31a5
MiguelViapiana <migvjung@gmial.com>
2024-10-10 16:11:10 -0300

    Navigation between Candidate and Vote screens

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -8,18 +8,20 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+
+	private static Scene mainScene;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Election ballot box");
 			primaryStage.show();
@@ -27,7 +29,12 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public static Scene getMainScene() {
+		return mainScene;
+
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,42 +1,68 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Menu;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{
-	
+public class MainViewController implements Initializable {
+
 	@FXML
 	private MenuItem menuItemVote;
-	
-	@FXML 
+
+	@FXML
 	private MenuItem menuItemNewCandidate;
-	
+
 	@FXML
 	private MenuItem menuItemListCandidate;
-	
+
 	@FXML
 	public void onMenuItemVoteAction() {
-		System.out.println("MenuVote");
+		loadView("/gui/Vote.fxml");
 	}
-	
+
 	@FXML
 	public void onMenuItemNewCandidateAction() {
-		System.out.println("MenuCandidate");
+		loadView("/gui/CandidateForm.fxml");
 	}
-	
+
 	@FXML
 	public void onMenuItemListCandidateAction() {
-		System.out.println("MenuCandidate");
+		loadView("/gui/CandidateList.fxml");
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
+
+	}
+
+	private void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVbox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVbox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVbox.getChildren());
+		} 
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }

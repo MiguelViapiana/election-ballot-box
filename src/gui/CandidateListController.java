ec9b6f27c1769fe3f280bc2780ef73bb89a66fd8
MiguelViapiana <migvjung@gmail.com>
2024-10-16 21:08:03 -0300

    button to delete made candidate

--- src/gui/CandidateListController.java
+++ src/gui/CandidateListController.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import application.Main;
+import db.DbIntegrityException;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
@@ -20,6 +22,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -104,6 +107,7 @@ public class CandidateListController implements Initializable, DataChangeListene
 		obsList = FXCollections.observableArrayList(list);
 		tableViewCandidate.setItems(obsList);
 		initEditButtons();
+		initRemoveButtons();
 	}
 	
 	private void createDialogForm(Candidate obj, String absoluteName, Stage parentStage) {
@@ -148,6 +152,39 @@ public class CandidateListController implements Initializable, DataChangeListene
 		});
 	}
 
+	public void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnREMOVE.setCellFactory(param ->new TableCell<Candidate, Candidate>(){
+			private final Button button = new Button("remove");
+			
+			@Override
+			protected void updateItem(Candidate obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if(obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+	}
+	
+	private void removeEntity(Candidate obj) {
+		Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
+
+		if (result.get() == ButtonType.OK) {
+			if (service == null) {
+				throw new IllegalStateException("Service was null");
+			}
+			try {
+				service.remove(obj);
+				updateTableView();
+			} catch (DbIntegrityException e) {
+				Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
+	}
 	@Override
 	public void onDataChanged() {
 		updateTableView();
